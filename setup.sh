f094074d9afb976a94935f44edca2c07cabb03ce
ngxson <thichthat@gmail.com>
2017-02-02 21:53:28 +0700

    Thu 02/02/2017-21-53-28

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-#example: ./setup.sh password
-echo '(google-chrome --kiosk file:///home/$USER/grandPC/runOnBoot.html)&' >> ~/.profile
-echo '(while true; do' >> ~/.profile
-echo '	if [ -f /home/$USER/Downloads/turnoff.txt ]; then' >> ~/.profile
-echo '		rm /home/$USER/Downloads/turnoff.txt' >> ~/.profile
-echo '		echo \"$1\" | sudo -S sync' >> ~/.profile
-echo '		echo \"$1\" | sudo -S poweroff' >> ~/.profile
-echo '	fi' >> ~/.profile
-echo 'done; sleep 5)&' >> ~/.profile
+(google-chrome --kiosk file:///home/$USER/grandPC/runOnBoot.html)&
+(while true; do
+	if [ -f /home/$USER/Downloads/turnoff.txt ]; then
+		rm /home/$USER/Downloads/turnoff.txt
+		echo "$1" | sudo -S sync
+		echo "$1" | sudo -S poweroff
+	fi
+	sleep 5;
+done;)&
