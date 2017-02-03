b89dcfe3ce326c2db4b9898fe4ba46e15f258300
ngxson <thichthat@gmail.com>
2017-02-03 22:52:10 +0700

    fix up

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-(google-chrome --kiosk file:///home/$USER/grandPC/runOnBoot.html)&
+(google-chrome --disable-infobars --disable-session-crashed-bubble --kiosk file:///home/$USER/grandPC/runOnBoot.html)&
 (while true; do
 	if [ -f /home/$USER/Downloads/turnoff.txt ]; then
 		rm /home/$USER/Downloads/turnoff.txt
