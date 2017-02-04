bec7b2a41559b7debc77adde5fdc63936118b597
ngxson <thichthat@gmail.com>
2017-02-04 17:12:41 +0700

    live now

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/google-chrome/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' ~/.config/google-chrome/Default/Preferences
+sed -i -e 's/("exit_type":\s*)"Crashed"/"exit_type":"None"/g' ~/.config/google-chrome/Default/Preferences
+sed -i -e 's/("exited_cleanly":\s*)false/"exited_cleanly":true/g' ~/.config/google-chrome/Default/Preferences
+
 (google-chrome --disable-infobars --disable-session-crashed-bubble --kiosk file:///home/$USER/grandPC/runOnBoot.html)&
 (while true; do
 	if [ -f /home/$USER/Downloads/turnoff.txt ]; then
@@ -7,3 +13,4 @@
 	fi
 	sleep 5;
 done;)&
+
